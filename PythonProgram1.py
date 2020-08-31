7d6931abf2f0f6a618890b631838f67ab36ec0e5
APARNAGADHIYA <aparnagadhiya1711@gmail.com>
2020-08-31 06:20:35 +0530

    Added First Python Program File

---
+++ PythonProgram1.py
@@ -0,0 +1,13 @@
+print("Hello to Python")
+name= input("What is your Name ")
+print("My name is ",name)
+amount=int(input("Enter the price of the item "))
+print("The price is",amount)
+print("The type of amount is ",type(amount))
+discount= 0.1*amount
+print("The discount offered is ",discount)
+sp= amount-discount
+print("The final selling price of the product is ", sp)
+num =int(10.45)
+print(num)
+"""This gives error num1= int("10.45") print(num1)"""
